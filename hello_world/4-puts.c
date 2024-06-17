505aab603d5cdd46d13f40ccf7f9039482589cc5
David9557 <9557@holbertonstudents.com>
2024-06-17 14:43:04 +0000

    Task 2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void)
+
 {
-	puts("Programmer, c'est comme construire un puzzle multilingue");
+	puts("\"Programmer, c'est comme construire un puzzle multilingue");
 
 	return (0);
 }
