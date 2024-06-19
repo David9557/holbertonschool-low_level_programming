f995d361e244bc8e687f8d94f993a1375b638979
David9557 <9557@holbertonstudents.com>
2024-06-19 09:11:59 +0000

    Task 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 6)
+		printf("%d and is less than 6 and not 0\n", n);
+	else if (n > 5)
+		printf("%d and is greater than 5\n", n);
+	else
+		printf("%d and is 0\n", n);
+	return (0);
+}
