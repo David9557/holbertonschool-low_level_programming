0fef0fdbb043bbea8bfc332be26fdbec8e12a005
David9557 <9557@holbertonstudents.comgit config --global user.name David9557./push.sh  Task>
2024-06-20 21:51:18 +0000

    Task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ ** main - Prints _putchar, followed by a new line.
+ **
+ **
+ ** Return: Always 0.
+ **/
 
 int main(void)
-
 {
-	("_putchar"\n);
+
+	int _putchar(char c);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
