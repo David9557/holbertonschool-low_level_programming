8de2d8698dfd2773b7e25e07024b0d3cb679fd19
david9557 <9557@holbertonstudents.com>
2024-06-23 12:36:04 +0000

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,16 @@
 void print_to_98(int n)
 {
 	while (n < 98)
-		{
-			printf("%d, ", n);
-			n++;
-		}
+	{
+		printf("%d, ", n);
+		n++;
+	}
 
 	while (n > 98)
-		{
-			printf("%d, ", n);
-			n--;
-		}
+	{
+		printf("%d, ", n);
+		n--;
+	}
 
 	if (n == 98)
 		printf("%d", n);
