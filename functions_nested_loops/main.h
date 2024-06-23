7cbf59b97a16e19d45f0b10b76bb012b3169c232
david9557 <9557@holbertonstudents.com>
2024-06-23 12:01:46 +0000

    Task 6

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,17 @@
-int main(void);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-char _putchar(char i);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
+#endif /* MAIN_H */
