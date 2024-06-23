78e8b90b95ddefe88f11dd08cee13b538d89d290
david9557 <9557@holbertonstudents.com>
2024-06-23 12:41:23 +0000

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
-			_putchar(':');		
+			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
