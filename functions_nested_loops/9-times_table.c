dc4d97686d84e37164f68847b5283ef046654e7c
david9557 <9557@holbertonstudents.com>
2024-06-23 12:38:45 +0000

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,27 +13,27 @@ void times_table(void)
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
+		{
+			product = (row * column);
+			if (column == 0)
 			{
-				product = (row * column);
-				if (column == 0)
-					{
-						_putchar('0' + product);
-					}
-				else if (product <= 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + product);
-					}
-				else if (product > 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar('0' + (product / 10));
-						_putchar('0' + (product % 10));
-					}
+				_putchar('0' + product);
 			}
-			_putchar('\n');
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
 	}
 }
