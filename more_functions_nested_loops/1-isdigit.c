94479c52f409134de6464a5da920bb98b66ced9b
David9557 <9557@holbertonstudents.com>
2024-06-24 08:16:28 +0000

    Task 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * _isdgit - regarde si c'est un chiffre 
+ * _isdgit - regarde si c'est un chiffre
  * Return: return 0 si ce n'est pas un chiffre compris entre 0 et 9
  * et 1 si sa l'est.
  */
