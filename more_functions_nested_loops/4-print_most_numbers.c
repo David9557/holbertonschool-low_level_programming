2990c6746e349bc44e4dacb4f10a8243b7501143
David9557 <9557@holbertonstudents.com>
2024-06-25 08:02:32 +0000

    Task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0 to 9, exept 2 and 4
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+
+		_putchar(c);
+	}
+	_putchar('\n');
+}
